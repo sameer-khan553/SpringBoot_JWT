789112e7b273c09c26a1d90376681ca1ae8f3768
Sameer Khan <155846059+sameer-khan553@users.noreply.github.com>
2026-02-20 11:54:17 +0530

    Updated: jwt with cookies

--- src/main/java/sameer/JWT/Controller/MyController.java
+++ src/main/java/sameer/JWT/Controller/MyController.java
@@ -4,6 +4,8 @@ package sameer.JWT.Controller;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.*;
 
 import javax.crypto.SecretKey;
@@ -16,7 +18,8 @@ public class MyController {
    private final SecretKey SIGNING_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
    @GetMapping("/genrate-token")
-   public String generateToken(@RequestParam String userName, @RequestParam String role) {
+   public String generateToken(@RequestParam String userName, @RequestParam String role ,
+                               HttpServletResponse response) {
        String token = Jwts.builder()
                .setSubject(userName)
                .claim("role", role)
@@ -24,7 +27,14 @@ public class MyController {
                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
                .signWith(SIGNING_KEY)
                .compact();
-       return "Token generated SuccessFully:"
-               + token;
+
+
+       Cookie cookie  = new Cookie("username",token);
+       cookie.setHttpOnly(true);       // js cannot access this cookies
+       cookie.setMaxAge(600);       // Expiry Time
+       cookie.setPath("/");     //  entire website can access
+       response.addCookie(cookie);
+       return "Jwt Store inside Cookie";
+
    }
 }
